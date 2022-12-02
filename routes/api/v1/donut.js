36389d45fec7de9811521f2e297be8d9b0fa664c
timvernaeve <r0754975@student.thomasmore.be>
2022-12-02 12:19:45 +0100

    feat(router/donut): changed route to include both order ID and donut ID

--- routes/api/v1/donut.js
+++ routes/api/v1/donut.js
@@ -3,8 +3,9 @@ var router = express.Router();
 var donutController = require('../../../controllers/api/v1/donut');
 
 router.get('/', donutController.getAll);
-router.get('/:id', donutController.getOne);
+router.get('/:id/:donut', donutController.getOne);
 router.post('/', donutController.create);
 router.delete('/:id', donutController.deleteDonut);
+router.put('/:id/:donut', donutController.updateDonut);
 
 module.exports = router;
