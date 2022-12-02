6368bbff802e75b3986e4508bb27664044530ca3
timvernaeve <r0754975@student.thomasmore.be>
2022-12-02 10:54:27 +0100

    feat(models/donut): add schema for donut order

---
+++ models/donut.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const DonutSchema = new Schema({
+
+        contact: {
+            mail : {
+                type: String,
+                required: true
+            },
+            phone : {
+                type: String,
+                required: true
+            },
+            name : {
+                type: String,
+                required: true
+            },
+            address : {
+                type: String,
+                required: true
+            },
+            city : {
+                type: String,
+                required: true
+            }
+        },
+        donuts: [{
+            glaze: {
+                type: String,
+                required: true
+            },
+            filling: {
+                type: String,
+                required: true
+            },
+            topping: {
+                type: String,
+                required: true
+            },
+            extra: {
+                type: String,
+                required: true
+            },
+            quantity: {
+                type: Number,
+                required: true
+            }
+        }],
+        card: {
+            shape: {
+                type: String,
+                required: true
+            },
+            url: {
+                type: String,
+                required: true
+            }
+        }
+
+
+});
+
+const Donut = mongoose.model('Donut', DonutSchema);
+
+module.exports = Donut;
