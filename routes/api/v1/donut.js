d145458337ac7ba8a3d190b2a48d428bf19eb2b8
R0754975 <r0754975@student.thomasmore.be>
2022-12-09 21:49:46 +0100

    feat(donut): add jwt authenthication to donut routes

--- routes/api/v1/donut.js
+++ routes/api/v1/donut.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('../../../passport/passport');
 var donutController = require('../../../controllers/api/v1/donut');
 
-router.get('/', donutController.getAll);
-router.get('/:id/:donut', donutController.getOne);
+router.get('/', passport.authenticate('jwt', {session: false}), donutController.getAll);
+router.get('/:id/:donut', passport.authenticate('jwt', {session: false}), donutController.getOne);
 router.post('/', donutController.create);
-router.delete('/:id', donutController.deleteOrder);
-router.delete('/:id/:donut', donutController.deleteDonut);
+router.delete('/:id', passport.authenticate('jwt', {session: false}), donutController.deleteOrder);
+router.delete('/:id/:donut', passport.authenticate('jwt', {session: false}), donutController.deleteDonut);
 router.put('/:id/:donut', donutController.updateDonut);
 
 module.exports = router;
