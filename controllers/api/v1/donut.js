93fb1b116aa360b4d6d20145f8ea25861cfc7332
R0754975 <r0754975@student.thomasmore.be>
2022-12-12 19:25:42 +0100

    feat: add updateOrder route for business updates

--- controllers/api/v1/donut.js
+++ controllers/api/v1/donut.js
@@ -1,24 +1,30 @@
+const cookieParser = require("cookie-parser");
 const Donut = require("../../../models/donut");
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const secret = process.env.SECRET;
 
 const getAll = (req, res) => {
     Donut.find({}, (err, donuts) => {
         if (err) {
             res.json({ err });
-        }  res.json({
+        } res.json({
             "status": "success",
-            "data" : donuts
+            "data": donuts
         });
     });
 };
 
 const getOne = (req, res) => {
-    Donut.findById({"_id": req.params.id}, (err, order) => {
+    Donut.findById({ "_id": req.params.id }, (err, order) => {
         let donut = order.donuts.id(req.params.donut);
         if (err) {
             res.json({ err });
-        }  res.json({
+        } res.json({
             "status": "success",
-            "data" : donut
+            "data": donut
         });
     });
 };
@@ -28,27 +34,32 @@ const create = (req, res) => {
     donut.contact = req.body.contact;
     donut.donuts = req.body.donuts;
     donut.card = req.body.card;
-    donut.save((err, donut) => {
-        if (err) {
-            res.json({ err });
-        } else {
-            res.json({
-                "status": "success",
-                "body": donut
-            })
-        }
-    });
-};
+    donut.save().then(result => {
+        let token = jwt.sign({
+            oid: result._id
+        }, secret);
+        res.json({
+            "status": "success",
+            "data": {
+                "donut": donut,
+                "token": token
+            }
 
+        })
+        }).catch(err => {
+            res.json({ err });
+        }
+    );
+}
 const deleteOrder = (req, res) => {
     Donut.findByIdAndDelete(req.params.id, (err, donut) => {
         if (err) {
             res.json({ err });
-        }  
+        }
         res.json({
             "status": "success",
-            "data" : {
-                "donut" : "order " + req.params.id +  " deleted"
+            "data": {
+                "donut": "order " + req.params.id + " deleted"
             }
         });
 
@@ -65,8 +76,8 @@ const deleteDonut = (req, res) => {
             }
             res.json({
                 "status": "success",
-                "data" : {
-                    "donut" : "donut " + req.params.donut +  " deleted"
+                "data": {
+                    "donut": "donut " + req.params.donut + " deleted"
                 }
             });
         });
@@ -75,32 +86,70 @@ const deleteDonut = (req, res) => {
 
 const updateDonut = (req, res) => {
     Donut.updateOne(
-        { 
-        _id : req.params.id,
-        donuts : { $elemMatch : { _id : req.params.donut } } 
-    }, 
-    { $set : {
-                "donuts.$.glaze" : req.body.glaze,
-                "donuts.$.filling" : req.body.filling,
-                "donuts.$.topping" : req.body.topping,
-                "donuts.$.extra" : req.body.extra,
-                "donuts.$.quantity" : req.body.quantity
-        }
-    }, (err, body) => {
-        if (err) {
-           res.json({ err });
-        }
-        res.json({
-            "status": "success",
-            "data" : {
-                "test" : req.body,
-                "donut" : "donut " + req.params.donut +  " updated"
+        {
+            _id: req.params.id,
+            donuts: { $elemMatch: { _id: req.params.donut } }
+        },
+        {
+            $set: {
+                "donuts.$.glaze": req.body.glaze,
+                "donuts.$.filling": req.body.filling,
+                "donuts.$.topping": req.body.topping,
+                "donuts.$.extra": req.body.extra,
+                "donuts.$.quantity": req.body.quantity
             }
+        }, (err, body) => {
+            if (err) {
+                res.json({ err });
+            }
+            res.json({
+                "status": "success",
+                "data": {
+                    "test": req.body,
+                    "donut": "donut " + req.params.donut + " updated"
+                }
+            });
         });
-    }); 
 };
 
-            
+
+const getOrder = (req, res) => {
+    Donut.findById(req.params.id, (err, donut) => {
+        if (err) {
+            res.json({ err });
+        } res.json({
+            "status": "success",
+            "data": donut
+        });
+    });
+};
+
+const updateOrder = (req, res) => {
+    Donut.updateOne(
+        {
+            _id: req.params.id
+        }
+        , {
+            $set: {
+                "contact.orderstatus": req.body.status,
+
+            }
+        }, (err, body) => {
+            if (err) {
+                res.json({ err });
+            }
+            res.json({
+                "status": "success",
+                "data": "order " + req.params.id + " updated"
+            });
+        });
+};
+
+const auth = (req, res, next) => {
+    res.json({
+        "status": "success"
+    })
+}
 
 
 module.exports.getAll = getAll;
@@ -109,3 +158,6 @@ module.exports.create = create;
 module.exports.deleteOrder = deleteOrder;
 module.exports.updateDonut = updateDonut;
 module.exports.deleteDonut = deleteDonut;
+module.exports.getOrder = getOrder;
+module.exports.updateOrder = updateOrder;
+module.exports.auth = auth;
--- routes/api/v1/donut.js
+++ routes/api/v1/donut.js
@@ -5,9 +5,12 @@ var donutController = require('../../../controllers/api/v1/donut');
 
 router.get('/', passport.authenticate('jwt', {session: false}), donutController.getAll);
 router.get('/:id/:donut', passport.authenticate('jwt', {session: false}), donutController.getOne);
+router.get('/:id', donutController.getOrder);
 router.post('/', donutController.create);
 router.delete('/:id', passport.authenticate('jwt', {session: false}), donutController.deleteOrder);
 router.delete('/:id/:donut', passport.authenticate('jwt', {session: false}), donutController.deleteDonut);
-router.put('/:id/:donut', donutController.updateDonut);
+router.put('/:id/:donut',  passport.authenticate('jwt', {session: false}), donutController.updateDonut);
+router.put('/:id',  passport.authenticate('jwt', {session: false}), donutController.updateOrder);
+router.post('/auth', passport.authenticate('jwt', {session: false}), donutController.auth);
 
 module.exports = router;
